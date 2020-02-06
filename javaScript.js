d35a1b4f64917c405b7b45e1abfbf9eb64dac891
Ryan Compton <ryan.wade.compton@gmail.com>
2020-02-05 20:30:29 -0700

    Style UV index. Complete weatherForecast().

--- javaScript.js
+++ javaScript.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
         var searchTerm = $("#search-value").val();//grab value in input search-value.
         $("#search-value").val("");//empty input field.
         weatherFunction(searchTerm);//call weatherFunction with searchTerm parameter.
-        //weatherForecast(searchTerm);// NOT functioning properly.
+        weatherForecast(searchTerm);// NOT functioning properly***.
     });
 
     var history = JSON.parse(localStorage.getItem("history")) || [];//grab item from local storage, if any.
@@ -26,7 +26,7 @@ $(document).ready(function () {
     });
 
     function weatherFunction(searchTerm) {
-        
+
         $.ajax({
             type: "GET",
             url: "https://api.openweathermap.org/data/2.5/weather?q=" + searchTerm + "&appid=9bbe868aa95e2e05ff8a18fa3fab1fc7&units=imperial",
@@ -49,8 +49,7 @@ $(document).ready(function () {
             var wind = $("<p>").addClass("card-text").text("Wind Speed: " + data.wind.speed + " MPH");
             var humid = $("<p>").addClass("card-text").text("Humidity: " + data.main.humidity + "%");
             var temp = $("<p>").addClass("card-text").text("Temperature: " + data.main.temp + " °F");
-            var uvIndex;
-            var uvColor;//uvColor not appending to page**
+            //uvColor not appending to page**
 
             var lon = data.coord.lon;
             var lat = data.coord.lat;
@@ -58,28 +57,29 @@ $(document).ready(function () {
             $.ajax({
                 type: "GET",
                 url: "https://api.openweathermap.org/data/2.5/uvi?appid=9bbe868aa95e2e05ff8a18fa3fab1fc7&lat=" + lat + "&lon=" + lon,
-                
+
 
             }).then(function (response) {
                 console.log(response);
-                uvIndex = $("<p>").addClass("card-text").text("UV Index: " + response.value);
-                console.log(response.value);
 
-                // if (uvIndex < 3) {
-                //     uvColor = "green";
-                // } else if (uvIndex < 6) {
-                //     uvColor = "yellow";
-                // } else if (uvIndex < 8) {
-                //     uvColor = "orange;"
-                // } else if (uvIndex < 11) {
-                //     uvColor = "red";
-                // } else {
-                //     uvColor = "violet";
-                // }
+                var uvColor;
+                var uvResponse = response.value;
+                var uvIndex = $("<p>").addClass("card-text").text("UV Index: ");
+                var btn = $("<span>").addClass("btn btn-sm").text(uvResponse);
 
+
+                if (uvResponse < 3) {
+                    btn.addClass("btn-success");
+                } else if (uvResponse < 7) {
+                    btn.addClass("btn-warning");
+                } else {
+                    btn.addClass("btn-danger");
+                }
 
                 cardBody.append(uvIndex);
-                // cardBody.append(uvColor);//NOT BUILT PROPERLY.
+                $("#today .card-body").append(uvIndex.append(btn));
+                $(uvIndex).css({ "background-color": uvColor });
+
             });
 
             // merge and add to page
@@ -98,21 +98,30 @@ $(document).ready(function () {
             type: "GET",
             url: "https://api.openweathermap.org/data/2.5/forecast?q=" + searchTerm + "&appid=9bbe868aa95e2e05ff8a18fa3fab1fc7&units=imperial",
 
-        }).then(function (number) {
-            console.log(number);
-            var titleFive = $("<h3>").addClass("card-title").text(new Date().toLocaleDateString());
-            var imgFive = $("<img>").attr("src", "https://openweathermap.org/img/w/" + number.list.weather[0].icon + ".png");
+        }).then(function (data) {
+            console.log(data);
+            $("#forecast").html("<h4 class=\"mt-3\">5-Day Forecast:</h4>").append("<div class=\"row deck\">");
 
-            var cardFive = $("<div>").addClass("card");
-            var cardBodyFive = $("<div>").addClass("card-body");
-            var humidFive = $("<p>").addClass("card-text").text("Humidity: " + number.list.main.humidity + "%");
-            var tempFive = $("<p>").addClass("card-text").text("Temperature: " + number.list.main.temp + " °F");
+            for (var i = 0; i < data.list.length; i++) {
 
-            //merge to div
-            titleFive.append(imgFive);
-            cardBodyFive.append(titleFive, tempFive, humidFive);
-            cardFive.append(cardBodyFive);
-            $(".card-deck").append(cardFive);
+                if (data.list[i].dt_txt.indexOf("15:00:00") !== -1) {
+
+                    var titleFive = $("<h3>").addClass("card-title").text(new Date(data.list[i].dt_txt).toLocaleDateString());
+                    var imgFive = $("<img>").attr("src", "https://openweathermap.org/img/w/" + data.list[i].weather[0].icon + ".png");
+
+                    // var colFive = $("<div>").addClass("col-md-2");//newly added to test.
+                    var cardFive = $("<div>").addClass("card bg-primary text-white");
+                    var cardBodyFive = $("<div>").addClass("card-body p-2");
+                    var humidFive = $("<p>").addClass("card-text").text("Humidity: " + data.list[i].main.humidity + "%");
+                    var tempFive = $("<p>").addClass("card-text").text("Temperature: " + data.list[i].main.temp + " °F");
+
+                    //merge to div
+                    titleFive.append(imgFive);
+                    cardBodyFive.append(titleFive, tempFive, humidFive);
+                    cardFive.append(cardBodyFive);
+                    $(".card-deck").append(cardFive);
+                }
+            }
         });
     }
 
